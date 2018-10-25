00c79938d20356cdc4913fad5e629b575950ae23
Gouda <gouda.clark@gmail.com>
2018-10-24 23:15:44 -0400

    FULL CRUD

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+
+pulp_fiction = Movie.create(
+    title: "Pulp Fiction",
+    genre: "action",
+    year: 1994,
+    synopsis: "The lives of two mob hit-men, a boxer, a gangsters wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    picture: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,686,1000_AL_.jpg"
+)
+
